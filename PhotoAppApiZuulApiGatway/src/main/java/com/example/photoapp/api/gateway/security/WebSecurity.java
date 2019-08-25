80b7fea3604563ebfaa8ea5dbbe4bfdf517b3d52
prasad <prasad.joshi1@gmail.com>
2019-08-25 10:17:54 +0530

    Actuator Changes

--- PhotoAppApiZuulApiGatway/src/main/java/com/example/photoapp/api/gateway/security/WebSecurity.java
+++ PhotoAppApiZuulApiGatway/src/main/java/com/example/photoapp/api/gateway/security/WebSecurity.java
@@ -33,6 +33,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 		.antMatchers(environment.getProperty("api.h2console.url.path")).permitAll()
 		.antMatchers(HttpMethod.POST,environment.getProperty("api.registration.url.path")).permitAll()
 		.antMatchers(HttpMethod.POST,environment.getProperty("api.login.url.path")).permitAll()
+		.antMatchers(environment.getProperty("api.zuul.actuator.url.path")).permitAll()
+		.antMatchers(environment.getProperty("api.users.actuator.url.path")).permitAll()
 		.anyRequest().authenticated()
 		.and()
 		.addFilter(new AuthorizationFilter(authenticationManager(), environment))
